bb2e4efc0be329476a51b7f9b0c178a8601685a3
itaruby_on_rails <itaruo93o@icloud.com>
2023-11-26 12:58:46 +0900

    45. Navigating Programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,8 @@
+import { useHistory } from 'react-router-dom'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+  const history = useHistory()
   function addMeetupHandler(meetupData) {
     fetch(
       'https://react-getting-started-cc103-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
@@ -11,7 +13,9 @@ function NewMeetupPage() {
           'Content-Type': 'application/json',
         },
       }
-    )
+    ).then(() => {
+      history.replace('/')
+    })
   }
   return (
     <section>
