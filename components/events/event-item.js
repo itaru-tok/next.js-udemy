45474bad1cc248f7d0a46bf23edc25fe3807ff4a
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-01 08:16:59 +0900

    79. Adding Buttons & Icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,9 @@
 import Link from 'next/link'
 
+import Button from '../ui/button'
+import AddressIcon from '../icons/address-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
+import DateIcon from '../icons/date-icon'
 import classes from './event-item.module.css'
 
 function EventItem(props) {
@@ -20,14 +24,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.action}>
-          <Link href={exploreLInk}>Explore Event</Link>
+          <Button link={exploreLInk}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
