73a5e30fe6bbee3f09a4b6badade10f21cb79cf5
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-02 12:53:03 +0900

    85. Extracting Data on the Catch-All Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,44 @@
+import { useRouter } from 'next/router'
+import getFilteredEvents from '../../dummy-data'
+
 function FilteredEventsPage() {
+  const router = useRouter()
+  const filteredData = router.query.slug
+
+  /*  If it is, it means that the data is still being loaded or has
+not been fetched yet. In this case, the code returns a loading message to indicate that the data is
+being fetched. */
+  if (!filteredData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = filteredData[0]
+  const filteredMonth = filteredData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>
+  }
+
+  console.log(filteredData)
   return (
     <div>
       <h1>Filtered Events</h1>
