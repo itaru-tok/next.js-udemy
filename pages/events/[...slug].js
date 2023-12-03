88e8b380182768bfc8b7963ea84c8d2bde5cb353
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-03 20:39:30 +0900

    125. Using Server-side Rendering (SSR)

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,42 +1,32 @@
 import { Fragment } from 'react'
 import { useRouter } from 'next/router'
 
-import { getFilteredEvents } from '../../dummy-data'
+import { getFilteredEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
 import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/button'
 import ErrorAlert from '../../components/ui/error-alert'
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
   const router = useRouter()
-  const filteredData = router.query.slug
+  // const filteredData = router.query.slug
 
   /*  If it is, it means that the data is still being loaded or has
 not been fetched yet. In this case, the code returns a loading message to indicate that the data is
 being fetched. */
-  if (!filteredData) {
-    return <p className="center">Loading...</p>
-  }
+  // if (!filteredData) {
+  //   return <p className="center">Loading...</p>
+  // }
 
-  const filteredYear = filteredData[0]
-  const filteredMonth = filteredData[1]
+  // const filteredYear = filteredData[0]
+  // const filteredMonth = filteredData[1]
 
-  const numYear = +filteredYear
-  const numMonth = +filteredMonth
+  // const numYear = +filteredYear
+  // const numMonth = +filteredMonth
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  })
+  const filteredEvents = props.events
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
+  if (props.hasError) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -62,7 +52,7 @@ being fetched. */
     )
   }
 
-  const date = new Date(numYear, numMonth - 1)
+  const date = new Date(props.date.year, props.date.month - 1)
 
   return (
     <Fragment>
@@ -70,6 +60,44 @@ being fetched. */
       <EventList items={filteredEvents} />
     </Fragment>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context
+  const filteredData = params.slug
+
+  const filteredYear = filteredData[0]
+  const filteredMonth = filteredData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return {
+      props: { hasError: true },
+    }
+  }
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  })
+
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  }
 }
 
 export default FilteredEventsPage
