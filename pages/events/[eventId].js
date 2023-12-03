dc196abc3875bcf971aa466749723b431a9f03ed
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-03 19:42:01 +0900

    123. Optimizing Data Fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,20 +1,18 @@
 import { Fragment } from 'react'
 
-import { getEventById } from '../../helpers/api-util'
-import { getAllEvents } from '../../helpers/api-util'
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util'
 import EventSummary from '../../components/event-detail/event-summary'
 import EventLogistics from '../../components/event-detail/event-logistics'
 import EventContent from '../../components/event-detail/event-content'
-import ErrorAlert from '../../components/ui/error-alert'
 
 function EventDetailPage(props) {
   const event = props.event
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     )
   }
 
@@ -44,19 +42,21 @@ export async function getStaticProps(context) {
     props: {
       event,
     },
+    revalidate: 30,
   }
 }
 // getStaticProps(context)を使う場合は、getStaticPaths()を使う
-// どのIDかわからないため、全てのIDを取得する
 // useRouter()を使う場合は、getServerSideProps()を使う
 export async function getStaticPaths() {
-  const events = await getAllEvents()
+  const events = await getFeaturedEvents()
 
   const paths = events.map((event) => ({ params: { eventId: event.id } }))
   return {
     paths: paths,
-    fallback: false,
-    /* specify that if a
+    /* The `fallback: 'blocking'` option in the `getStaticPaths` function is used to specify the fallback
+  behavior for pages that are not statically generated at build time. */
+    fallback: 'blocking',
+    /* The `fallback: false` specify that if a
     requested page doesn't have a corresponding pre-generated static page, Next.js should return a
     404 page instead of trying to generate the page on the fly. */
   }
