6e032213bb7bdb27e3894c58ba36205b6fcf9384
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-03 21:25:19 +0900

    126. Adding Client-Side Data Fetching

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,32 +1,59 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
 
-import { getFilteredEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
 import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/button'
 import ErrorAlert from '../../components/ui/error-alert'
 
-function FilteredEventsPage(props) {
+function FilteredEventsPage() {
+  const [loadedEvents, setLoadedEvents] = useState()
   const router = useRouter()
-  // const filteredData = router.query.slug
+
+  const filteredData = router.query.slug
+
+  const { data, error } = useSWR(
+    'https://next-udemy-section6-default-rtdb.asia-southeast1.firebasedatabase.app/events.json'
+  )
+
+  useEffect(() => {
+    if (data) {
+      const events = []
+
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        })
+      }
+
+      setLoadedEvents(events)
+    }
+  }, [data])
 
   /*  If it is, it means that the data is still being loaded or has
 not been fetched yet. In this case, the code returns a loading message to indicate that the data is
 being fetched. */
-  // if (!filteredData) {
-  //   return <p className="center">Loading...</p>
-  // }
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>
+  }
 
-  // const filteredYear = filteredData[0]
-  // const filteredMonth = filteredData[1]
+  const filteredYear = filteredData[0]
+  const filteredMonth = filteredData[1]
 
-  // const numYear = +filteredYear
-  // const numMonth = +filteredMonth
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
 
-  const filteredEvents = props.events
-
-  if (props.hasError) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -38,6 +65,14 @@ being fetched. */
       </Fragment>
     )
   }
+
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date)
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    )
+  })
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -52,7 +87,7 @@ being fetched. */
     )
   }
 
-  const date = new Date(props.date.year, props.date.month - 1)
+  const date = new Date(numYear, numMonth - 1)
 
   return (
     <Fragment>
@@ -62,42 +97,42 @@ being fetched. */
   )
 }
 
-export async function getServerSideProps(context) {
-  const { params } = context
-  const filteredData = params.slug
+// export async function getServerSideProps(context) {
+//   const { params } = context
+//   const filteredData = params.slug
 
-  const filteredYear = filteredData[0]
-  const filteredMonth = filteredData[1]
+//   const filteredYear = filteredData[0]
+//   const filteredMonth = filteredData[1]
 
-  const numYear = +filteredYear
-  const numMonth = +filteredMonth
+//   const numYear = +filteredYear
+//   const numMonth = +filteredMonth
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
-    return {
-      props: { hasError: true },
-    }
-  }
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  })
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     return {
+//       props: { hasError: true },
+//     }
+//   }
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   })
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  }
-}
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   }
+// }
 
 export default FilteredEventsPage
