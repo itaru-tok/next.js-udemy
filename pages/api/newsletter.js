37eb02ce8851cd295161c399c12299193350ddfb
itaruby_on_rails <itaruo93o@icloud.com>
2023-12-05 20:22:53 +0900

    added error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,23 +1,31 @@
-import { MongoClient } from 'mongodb'
+import { connectDatabase, insertDocument } from '../../helpers/db-util'
 
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email
+    debugger
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address.' })
       return
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://Itaru:fJXFiVaTscdd9shr@cluster0.srgvels.mongodb.net/events?retryWrites=true&w=majority'
-    )
+    let client
 
-    const db = client.db()
+    try {
+      client = await connectDatabase()
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' })
+      return
+    }
 
-    await db.collection('newsletter').insertOne({ email: userEmail })
-
-    client.close()
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail })
+      client.close()
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' })
+      return
+    }
 
     res.status(201).json({ message: 'Signed up!' })
   }
